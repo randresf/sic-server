eaf9879a07c321ad4045013a1518e483812b8662
Lucho <l.contreras534@pascualbravo.edu.co>
2020-11-22 17:44:34 -0500

    field userId dont saved

--- src/resolvers/questions.ts
+++ src/resolvers/questions.ts
@@ -20,7 +20,7 @@ class QuestionResponse {
 @InputType()
 class QuestionType {
   @Field()
-  citizenId!: string;
+  UserId!: string;
   @Field()
   questionId!: string;
   @Field()
@@ -39,7 +39,13 @@ export class QuestionResolver {
   async registrerQuestion(
     @Arg("questions", () => [QuestionType]) questions: [QuestionType]
   ): Promise<QuestionResponse> {
-    await Question.insert(questions);
-    return { saved: true };
+    console.log(questions);
+    try {
+      await Question.insert(questions);
+
+      return { saved: true };
+    } catch (error) {
+      return { error };
+    }
   }
 }
